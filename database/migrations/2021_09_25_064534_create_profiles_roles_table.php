1aff7bd640dfd265f5c5754f3e70fa76e10e181f
ali x <newin4mations@gmail.com>
2021-09-25 11:23:01 +0330

    add controllers for authentications

--- database/migrations/2021_09_25_064534_create_profiles_roles_table.php
+++ database/migrations/2021_09_25_064534_create_profiles_roles_table.php
@@ -15,6 +15,13 @@ class CreateProfilesRolesTable extends Migration
     {
         Schema::create('profiles_roles', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger("profile_id")->index();
+            $table->foreign("profile_id")->references("id")->on("profiles");
+
+            $table->unsignedBigInteger("role_id")->index();
+            $table->foreign("role_id")->references("id")->on("roles");
+
             $table->timestamps();
         });
     }
