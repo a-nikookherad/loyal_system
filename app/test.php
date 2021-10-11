aeb17f1ea1fa40c29b7b3dec76ddff0d00f7c1b6
ali x <newin4mations@gmail.com>
2021-10-11 16:41:58 +0330

    add test file for email

--- app/test.php
+++ app/test.php
@@ -1,6 +1,7 @@
 <?php
-//Mail::mailer("smtp")->to("alinikokherad@gmail.com")->send(new \App\Mail\NewUserNotification);
-require '../vendor/autoload.php';
+Mail::mailer("smtp")->to("alinikokherad@gmail.com")->send(new \App\Mail\NewUserNotification);
+dd("done");
+/*require '../vendor/autoload.php';
 use \Mailjet\Resources;
 $mj = new \Mailjet\Client('5c04b68c96cfc690e6aa20464160b810','427f879f251879120692090af310cc41',true,['version' => 'v3.1']);
 $body = [
@@ -25,4 +26,4 @@ $body = [
 ];
 $response = $mj->post(Resources::$Email, ['body' => $body]);
 $response->success() && var_dump($response->getData());
-dd("done");
+dd("done");*/
